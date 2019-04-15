0fa4dac7c6ad831989703c64e061acffe2a36f6a
187016858 <a187016858@gmail.com>
2019-04-15 22:38:40 +0800

    Updates

---
+++ precache-manifest.b454d56f17e66f2c6d180f70222b799e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "d74b1fbf0c49fac736e2",
+    "url": "/my-app/static/js/main.c0c9394a.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/my-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d74b1fbf0c49fac736e2",
+    "url": "/my-app/static/css/main.13533143.chunk.css"
+  },
+  {
+    "revision": "152836cd1235ae86e93ddc855a593acf",
+    "url": "/my-app/index.html"
+  }
+];
